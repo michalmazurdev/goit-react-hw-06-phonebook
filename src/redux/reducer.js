8afab3c899e33c40d79e5a205387a96703c4b174
michalmazurdev <m.mazur40@gmail.com>
2023-07-15 23:03:19 +0200

    local storage functionality added

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,20 +1,19 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { addContact, removeContact, setFilter } from './actions';
 
-const contactsInitialState = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
+
+const contactsInitialState = contacts;
 
 export const contactsReducer = createReducer(contactsInitialState, {
   [addContact]: (state, action) => {
     state.push(action.payload);
+    localStorage.setItem('contacts', JSON.stringify(state));
   },
   [removeContact]: (state, action) => {
     const index = state.findIndex(contact => contact.id === action.payload);
     state.splice(index, 1);
+    localStorage.setItem('contacts', JSON.stringify(state));
   },
 });
 
