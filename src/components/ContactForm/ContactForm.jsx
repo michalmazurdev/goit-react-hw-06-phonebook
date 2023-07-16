36a6a02ba6679a7ea635235cf80cde922e22497b
michalmazurdev <m.mazur40@gmail.com>
2023-07-16 13:11:57 +0200

    input patterns fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -27,7 +27,7 @@ export const ContactForm = () => {
           className={css.input}
           type="text"
           name="name"
-          pattern="^[a-zA-Z]+(([' \u2013][a-zA-Z])?[a-zA-Z]*)*$"
+          pattern="^[A-Za-z.'\- ]+$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
@@ -38,7 +38,7 @@ export const ContactForm = () => {
           className={css.input}
           type="tel"
           name="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          pattern="^\+?\d{1,4}?\s?\(?\d{1,4}?\)?\s?\d{1,4}\s?\d{1,4}\s?\d{1,9}$"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
         />
